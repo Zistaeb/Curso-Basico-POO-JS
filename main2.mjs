43394394c3712b5573d91dfe4b7678be84e69809
Betty <bperezatencio@gmail.com>
2023-08-09 14:16:41 -0300

    Módulos de ECMAScript 6

--- main2.mjs
+++ main2.mjs
@@ -1,3 +1,31 @@
+function videoPlay(id) {
+    const urlSecreta = "https://platzisecreto.com/" + id;
+    console.log("Se está reproduciendo desde la url" + urlSecreta);
+};
+
+function videoStop(id) {
+    const urlSecreta = "https://platzisecreto.com/" + id;
+    console.log("Pausamos la url" + urlSecreta);
+};
+
+export class PlatziClass{
+    constructor({
+        name, 
+        videoID,
+    }) {
+        this.name = name;
+        this.videoID = videoID;
+    };
+
+    reproducir() {
+        videoPlay(this.videoID);
+    };
+
+    pausar() {
+        videoStop(this.videoID);
+    };
+};
+
 class Course {
     constructor({
         name,
